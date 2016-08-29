d64fc28f6bef5ffb4c126621ee77bac1678bafef
wujunwei <wujunwei@le.com>
2016-08-29 18:29:52 +0800

    大数据学习, 熟悉maven的pom.xml的相关配置

---
+++ src/main/java/Hello.java
@@ -0,0 +1,8 @@
+/**
+ * Created by wujunwei on 2016/8/29.
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
---
+++ src/main/java/Spark.java
@@ -0,0 +1,8 @@
+/**
+ * Created by wujunwei on 2016/8/29.
+ */
+public class Spark {
+    public static void main(String[] args) {
+        System.out.println("Spark");
+    }
+}
