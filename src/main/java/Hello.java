759ac183e21fe638881a9aa77a301790de4cc032
wujunwei <1399952803@qq.com>
2016-08-30 17:13:05 +0800

    项目改为pom.xml聚合的多模块模式, 模块的pom.xml继承主项目的pom.xml

--- src/main/java/Hello.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by wujunwei on 2016/8/29.
- */
-public class Hello {
-    public static void main(String[] args) {
-        System.out.println("Hello");
-    }
-}
--- src/main/java/Spark.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by wujunwei on 2016/8/29.
- */
-public class Spark {
-    public static void main(String[] args) {
-        System.out.println("Spark");
-    }
-}
